4ea25151955fb059e63cfed3a73d91ad73b36025
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2022-04-10 14:11:03 +0800

    没啥

---
+++ codemaoDesktop/scripts/check.bat
@@ -0,0 +1,23 @@
+echo 请务必安装nodejs
+echo 检查nodejs版本
+node -v
+echo 检查yarn版本
+yarn -v
+echo 作者版本:
+echo node 14.15.3
+echo yarn 1.22.17
+
+echo ^[[32m 
+___________________________\n
+|                           |\n
+|          使用指引          |\n
+|                           |\n
+|     yarn dev :开发调试     |\n
+|    yarn build :搭建应用    |\n
+|                           |\n
+-----------------------------
+;^[[0m
+
+echo ^[[31m记得将路径切换到上一级哦;^[[0m"
+
+echo 注:windows版未测试,也许会有bug
